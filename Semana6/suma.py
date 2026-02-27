6444caaf67add2e1f7483cf366cb562fb6a504f6
ad0456032026-pixel <ad04560320@unab.edu.svgit config --global user.email ad04560320@unab.edu.sv>
2026-02-26 20:09:08 -0600

    se agrega la suma de python en visual studio code

---
+++ Semana6/suma.py
@@ -0,0 +1,42 @@
+# es un comenytario
+
+## para declarar una variable
+nombre = "Ever"
+print(nombre)
+edad = 40
+print(nombre, edad, "years old")
+# binario = 0
+# binario = 1
+
+
+# inmutabilidad : pero siempre deben conservar su tipo
+# string = letras
+# objeto = 012345
+
+
+## direccion # 0x
+##tipo => numerico o string (booleano, objeto)
+## tipo numero => prepara a la memoria
+## para guardar un tipo de dato string hexa.
+
+## textos largos vamos a utilizar las comillas triples
+texto_largo = """jijijija soy un  gamer y me encanta jugar videojuegos, me gusta jugar con mis amigos y pasar tiempo en linea como un antisocial"""
+print(texto_largo)
+## las comillas triples se usan para textos largos
+## mantiene el formato el formato de lo que se copia entre ellas
+## al momento de compilarlas
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 + numero2
+print("El resultado de la suma es: ", resultado)
+
+resultado = numero1 - numero2
+print("El resultado de la resta es: ", resultado)
+
+resultado = numero1 / numero2
+print("El resultado de la division es: ", resultado)
+
+resultado = numero1 * numero2
+print("El resultado de la multiplicacion es: ", resultado)
