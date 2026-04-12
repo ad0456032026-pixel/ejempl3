b2d899bd5e417f7ae0221061644e9881dbd3be60
ad0456032026-pixel <ad0456032026@unab.edu.sv>
2026-04-11 23:51:40 -0600

    parcial 02 fundamentos de programación, ejercicio 8, conteo en bloque de texto y separación por líneas

---
+++ semana11Parcial2/ejercicio8.py
@@ -0,0 +1,12 @@
+poema = """Caminante, son tus huellas
+el camino y nada más;
+Caminante, no hay camino."""
+
+conteo_a = poema.count("a")
+
+lista_oraciones = poema.splitlines()
+
+# Muestra resultados
+print("Bloque de texto original:\n" + poema + "\n")
+print("Cantidad de letras 'a':", conteo_a)
+print("Lista de oraciones:", lista_oraciones)
