19ded26511a2091381bd6bd44b5440f025d6286a
ad0456032026-pixel <ad0456032026@unab.edu.sv>
2026-04-11 23:52:01 -0600

    parcial 02 fundamentos de programación, ejercicio 9, reemplazar texto y convertir a mayúsculas

---
+++ semana11Parcial2/ejercicio9.py
@@ -0,0 +1,10 @@
+texto = "Any time y Anytime"
+
+texto_reemplazado = texto.replace("Any time", "Always")
+
+texto_final = texto_reemplazado.upper()
+
+# Muestraa resultados
+print("Texto original:", texto)
+print("Texto después del reemplazo:", texto_reemplazado)
+print("Texto final en mayúsculas:", texto_final)
