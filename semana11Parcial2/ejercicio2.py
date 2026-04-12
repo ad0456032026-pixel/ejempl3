1aa23596d1305c8afd86f764abab43c469903523
ad0456032026-pixel <ad0456032026@unab.edu.sv>
2026-04-11 23:48:58 -0600

    parcial 02 fundamentos de programación, ejercicio 2, formato título y reemplazo de texto

---
+++ semana11Parcial2/ejercicio2.py
@@ -0,0 +1,9 @@
+texto = "bryan alas"
+
+texto_titulo = texto.title()
+
+texto_final = texto_titulo.replace("Bryan", "Alas")
+
+print("Texto original:", texto)
+print("Texto modificado (title):", texto_titulo)
+print("Texto final resultante:", texto_final)
