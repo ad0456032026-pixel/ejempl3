bdb9f0cf4b0143ef10875dc9f86eb28e656710fc
ad0456032026-pixel <ad0456032026@unab.edu.sv>
2026-04-11 23:49:37 -0600

    parcial 02 fundamentos de programación, ejercicio 3, remover prefijo y convertir a mayúsculas

---
+++ semana11Parcial2/ejercicio3.py
@@ -0,0 +1,10 @@
+texto = "ING. Bryan Alas"
+
+sin_prefijo = texto.removeprefix("ING. ")
+
+texto_final = sin_prefijo.upper()
+
+# Muestra resultados
+print("Texto original:", texto)
+print("Texto sin prefijo:", sin_prefijo)
+print("Texto final en mayúsculas:", texto_final)
